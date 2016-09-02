9ae4aaae79514876949f060dee39cc4b0e090a97
Alessandro Vullo <avullo@ebi.ac.uk>
2016-09-02 15:32:10 +0100

    Converter generates milestones and logs collections plus the corresponding aggregator pages.

--- text_to_markdown.pl
+++ text_to_markdown.pl
@@ -10,13 +10,104 @@ use Data::Dumper;
 
 my $log_dir = 'logs';
 my $milestones_dir = 'milestones';
+my $DH;
 
-opendir(my $DH, $log_dir) || die "Can't open $log_dir: $!";
-my @wp_logs = grep { /^wp.+?[^\~]$/ && -f "$log_dir/$_" } readdir($DH);
-closedir $DH;
-
+# parse milestones for each WP and create the corresponding pages with md
 opendir($DH, $milestones_dir) || die "Can't open $milestones_dir: $!";
 my @wp_milestones = grep { /^wp.+?[^\~]$/ && -f "$milestones_dir/$_" } readdir($DH);
+closedir $DH;
+
+# make data directory to store status data for each WP
+`mkdir -p _data`;
+
+my $milestones;
+my %months = 
+  (
+   '01' => 'January',
+   '02' => 'February',
+   '03' => 'March',
+   '04' => 'April',
+   '05' => 'May',
+   '06' => 'June',
+   '07' => 'July',
+   '08' => 'August',
+   '09' => 'September',
+   '10' => 'October',
+   '11' => 'November',
+   '12' => 'December'
+  );
+
+unlink "_data/wp.yml";
+my %wps = 
+  (
+   'wp012' => 'Archives',
+   'wp03' => 'FTP',
+   'wp04' => 'Public MySQL',
+   'wp056' => 'Production (incl. genebuild)',
+   'wp07' => 'Web',
+   'wp08' => 'Outreach'
+  );
+
+foreach my $milestones_file (@wp_milestones) {
+  my $wp = (split /-/, $milestones_file)[0];
+  
+  # TEMP
+  # skip if wp08, not yet converted
+  next if $wp =~ /wp08/;
+
+  # just do it, do not die, this is implemented wrongly either in Perl or in shell
+  `mkdir -p "_${wp}_milestones"`; # or die "Could create dir _$wp: $!\n";
+  
+  # parse log to get content organised by date
+  my $file = "$milestones_dir/" . $milestones_file;
+  open my $FH, "<$file" or die "Cannot open file $file: $!\n";
+  my ($date, $log, $milestones);
+  while (my $line = <$FH>) {
+    if ($line =~ /^Objective:\s(.+?)$/i) {
+      $milestones->{goal} = $1;
+    } elsif ($line =~ /^State:\s(.+?)$/) {
+      defined $1 or die "Couldn't get state for WP $wp";
+      $milestones->{state} = $1;
+    } elsif ($line =~ /^(\d{4}-\d\d-\d\d)/) { # new monthly report
+      chomp $line;
+      $date = $1;
+      $log = 1;
+    } else {
+      $milestones->{log}{$date} .= $line
+	if defined $date and $log;
+    }
+  } 
+  close $FH;
+
+  # generate a collection under each wp collection dir from each date content
+  # `mkdir -p "_$wp/milestones"`;
+  foreach my $date (sort keys %{$milestones->{log}}) {
+    $date =~ /^\d{4}-(\d\d)-\d\d/;
+    # title is alphanumeric month
+    my $title = $months{$1};
+    open my $MH, sprintf ">_%s_milestones/%s.md", $wp, $date  or die "Cannot open collection item for WP $wp and date $date: $!\n";
+    print $MH <<CONTENT;
+---
+layout: post
+wp: $wp
+date: $date
+title: $title
+---
+
+$milestones->{log}{$date}
+CONTENT
+    close $MH;
+  }
+
+  # add WP state, objective to wp data
+  open my $DH, ">>_data/wp.yml" or die "Could not open data file: $!\n";
+  print $DH sprintf "%s:\n  name: %s\n  goal: %s\n  state: %s\n\n", $wp, $wps{$wp}, $milestones->{goal}, $milestones->{state};
+  close $DH;
+}
+
+# parse WP logs and create the corresponding blog posts and collection items
+opendir($DH, $log_dir) || die "Can't open $log_dir: $!";
+my @wp_logs = grep { /^wp.+?[^\~]$/ && -f "$log_dir/$_" } readdir($DH);
 closedir $DH;
 
 foreach my $log_file (@wp_logs) {
@@ -25,9 +116,6 @@ foreach my $log_file (@wp_logs) {
   # TEMP
   # skip if wp56/wp08, not yet converted
   next if $wp =~ /wp(056|08)/;
-
-  # just do it, do not die, this is implemented wrongly either in Perl or in shell
-  `mkdir -p "_$wp"`; # or die "Could create dir _$wp: $!\n";
   
   # parse log to get content organised by date
   my $file = "$log_dir/" . $log_file;
@@ -51,13 +139,6 @@ foreach my $log_file (@wp_logs) {
   } 
   close $FH;
  
-  # print Dumper $posts;
-  # print join("\n", sort keys %{$posts}), "\n";
-  # foreach my $date (sort keys %{$posts}) {
-  #   printf "%s - %s\n%s\n\n", $date, $posts->{$date}{title}, $posts->{$date}{content}
-  # }
-  #exit;
-
   # generate posts from each date content
   foreach my $date (sort keys %{$posts}) {
     my $normalised_title = $posts->{$date}{title};
@@ -75,7 +156,8 @@ CONTENT
     close $PH;
 
     # and now items in the wp log collection
-    open $PH, sprintf ">_%s/%s-%s.md", $wp, $date, $normalised_title or die "Cannot open $wp collection item md for date $date: $!\n";
+    `mkdir -p "_${wp}_logs"`;
+    open $PH, sprintf ">_%s_logs/%s-%s.md", $wp, $date, $normalised_title or die "Cannot open $wp collection item md for date $date: $!\n";
     print $PH <<CONTENT;
 ---
 layout: post
@@ -86,7 +168,6 @@ date: $date
 $posts->{$date}{content}
 CONTENT
     close $PH;
-  }
-
-  
+  }  
 }
+
